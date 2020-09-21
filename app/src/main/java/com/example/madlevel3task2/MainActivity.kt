ef0ed9d32bf7d48367857d0314db8ea7f90924c2
Sven van Putten <svenvanputten@gmail.com>
2020-09-21 10:45:20 +0200

    Added fragments and actions to the nav_graph.xml file and created a click listener in the MainActivity class

--- app/src/main/java/com/example/madlevel3task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel3task2/MainActivity.kt
@@ -6,13 +6,29 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // Navigate to the add portal fragment when clicking the button
+        fab.setOnClickListener {
+            navController.navigate(
+                    R.id.action_portalsFragment_to_addPortalFragment
+            )
+        }
+
+        fabToggler()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,6 +44,19 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * Toggle the fragment action button based on the current destination
+     */
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.addPortalFragment)) {
+                fab.hide()
+            } else {
+                fab.show()
+            }
         }
     }
 }
