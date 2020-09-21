ac7378651203e55cc4429766ba313db8b1a38acd
Sven van Putten <svenvanputten@gmail.com>
2020-09-21 15:08:24 +0200

    Made the Portal data class parcelable

--- app/src/main/java/com/example/madlevel3task2/classes/Portal.kt
+++ app/src/main/java/com/example/madlevel3task2/classes/Portal.kt
@@ -1,6 +1,10 @@
 package com.example.madlevel3task2.classes
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Portal(
     var title: String,
     var url: String,
-)
+) : Parcelable
